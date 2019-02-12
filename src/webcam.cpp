1332fe0c016054fa43cb5e9154234c3018dbd45e
Mislav Novakovic <mislav.novakovic@gmail.com>
2019-02-12 23:35:11 +0100

    recalculate avrage value of red pixels and show it
    Signed-off-by: Mislav Novakovic <mislav.novakovic@gmail.com>

--- src/webcam.cpp
+++ src/webcam.cpp
@@ -42,30 +42,31 @@ int main()
         const cv::Mat face = gray(*face_i);
         const cv::Mat faceMat(original, *face_i);
         const cv::Mat face_resized;
-        rectangle(original, *face_i, CV_RGB(0, 255, 0), 1);
+        cv::rectangle(original, *face_i, CV_RGB(0, 255, 0), 1);
         const int pos_x = std::max(face_i->tl().x - 10, 0);
         const int pos_y = std::max(face_i->tl().y - 10, 0);
 
         // Convert from BGR to HSV colorspace
-        cvtColor(faceMat, face_HSV, cv::COLOR_BGR2HSV, 3);
+        cv::cvtColor(faceMat, face_HSV, cv::COLOR_BGR2HSV, 3);
         // Detect the object based on HSV Range Values
-        inRange(face_HSV, cv::Scalar(0, 100, 30), cv::Scalar(5, 255, 255), face_threshold);
+        cv::inRange(face_HSV, cv::Scalar(0, 100, 30), cv::Scalar(5, 255, 255), face_threshold);
         cv::Mat face_masked;
         cv::copyTo(faceMat, face_masked, face_threshold);
 
-        //if (keypoints.empty()) { continue; }
-        //const auto sum_red = std::accumulate(keypoints.begin(), keypoints.end(), 0, [&original](unsigned int prev, cv::KeyPoint kp) {
-        //    return prev + original.at<cv::Vec3b>(kp.pt)[2];
-        //});
+        int size_red = std::accumulate(face_threshold.begin<cv::Vec3b>(), face_threshold.end<cv::Vec3b>(), 0, [](unsigned int prev, cv::Vec3b kp) {
+            return (kp[0] > 0) ? prev + 1 : prev;
+        });
+        int sum_red = std::accumulate(face_masked.begin<cv::Vec3b>(), face_masked.end<cv::Vec3b>(), 0, [](unsigned int prev, cv::Vec3b kp) {
+            return prev + kp[2];
+        });
 
-        int sum_red = 1;
-        int av_red = static_cast<int>(static_cast<float>(sum_red) / static_cast<float>(1));
+        int av_red = static_cast<int>(static_cast<float>(sum_red) / static_cast<float>(size_red));
 
         const std::string box_text =
-            std::string("Face pulse ") + std::to_string(av_red) + std::string(" num of pix TODO");
+            std::string("Face pulse ") + std::to_string(av_red) + std::string(" num of pix ") + std::to_string(size_red);
         cv::putText(original, box_text, cv::Point(pos_x, pos_y), cv::FONT_HERSHEY_PLAIN, 1.0, CV_RGB(0, 255, 0), 2);
 
-        cv::imshow("test2", face_masked);
+        cv::imshow("test2", original);
         if (cv::waitKey(30) >= 0) { break; }
     }
     return 0;
